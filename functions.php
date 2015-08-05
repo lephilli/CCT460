075cc77323f308fbf02adcf063a2e08219a7217f
lephilli <mail2philly@gmail.com>
2015-08-05 01:12:11 -0400

    functions.php
    - update

--- functions.php
+++ functions.php
@@ -27,3 +27,29 @@ add_action( 'widgets_init', 'child_widgets_init');
 
 // Footer Menu ------------------------------------------------------------------------>
 register_nav_menus (array ('secondary'=>__( 'Footer Menu' ),));
+
+// Read More [...] -> Excerpt Replacement --------------------------------------------->
+function custom_excerpt_more( $more ) {
+	return '';
+}
+add_filter( 'excerpt_more', 'custom_excerpt_more' );
+
+//  Posts_Navigation Newer/Older ------------------------------------------------------>
+function posts_navigation() {
+	// Don't print empty markup if there's only one page.
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+		return;
+	}
+	?>
+	<nav class="navigation posts-navigation" role="navigation">
+		<h2 class="screen-reader-text"><?php _e( 'Posts navigation', 'codediva' ); ?></h2>
+		<div class="nav-links">
+
+			
+			<div class="nav-previous"><?php next_posts_link(__( '&#8610; Left posts', 'underscores' ) ); ?></div>
+			
+			
+			<div class="nav-next"><?php previous_posts_link( __( 'Right posts &#8611;', 'underscores' )  ); ?></div>
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	<?php
